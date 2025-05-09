98376ae6ebc6a6ea355f7b8cdcc99e046bc479a8
Alex Young <45477688+alex-l-young@users.noreply.github.com>
2025-05-09 19:14:41 -0400

    new daq code

--- analysis/lab_system/data_acquisition/data_collection.m
+++ analysis/lab_system/data_acquisition/data_collection.m
@@ -13,21 +13,43 @@ fault_level = 0; % 0 for none, 1 for 20%, 2 for 40%
 drained = 0; % 0 for not drained, 1 for drained
 impulse_length = 1; % 0 is shortest  !!! check how many seconds !!!
 
-% Trigger valve.
+% Trigger valve to fill tank.
 %************************************
 dq = daq("ni");
 
 % Output trigger signal.
 ch0_out = addoutput(dq, "Dev1", "ao0", "Voltage");
+ch2 = addinput(dq, "Dev1", "ai2", "Voltage"); % bucket sensor
+ch2.TerminalConfig = "SingleEnded";
 
-% Trigger signal.
+% Trigger signal for filling up tank.
 pause(1)
 write(dq, 0);
 write(dq, 4);
 pause(0.1)
 write(dq, 0);
 pause(1)
-disp('TRIGGERED')
+disp('FILLING STARTED')
+
+% Wait for tank height to reach "full"
+% ************************************
+% Then send another trigger signal
+
+threshold = 1.5; % volts
+
+while true
+    tank_val = read(dq);
+
+    if tank_val <= threshold
+        write(dq, 4);
+        pause(0.1)
+        write(dq, 0);
+        disp('FILLING STOPPED')
+        break;
+    end
+
+    pause(0.1);
+end
 
 % Data Collection.
 %***********************************
@@ -48,7 +70,7 @@ ch2.TerminalConfig = "SingleEnded";
 % collect data
 dt_sensor = 0.03; % Sensor sampling rate.
 fs = 1/dt_sensor; % daq sampling rate (Hz)
-dt = 600; % trial length (s)
+dt = 20; % trial length (s)
 dq.Rate = fs;
 [data, time, start] = read(dq, seconds(dt), OutputFormat="Matrix");
 V_ai0 = data(:,1);
@@ -72,14 +94,14 @@ xlabel('Time (s)')
 ylabel('Voltage (V)')
 
 %% write to spreadsheet
-tab = table(time, V_ai0, V_ai1, V_ai2);
-date = string(datetime('now', 'Format', 'yyyy-MM-dd''_''HH-mm-ss'));
-path = "sensor_data\";
-filename = date + "_" + "sensor_data" + ".csv";
-writetable(tab, fullfile(path,filename));
+%tab = table(time, V_ai0, V_ai1, V_ai2);
+%date = string(datetime('now', 'Format', 'yyyy-MM-dd''_''HH-mm-ss'));
+%path = "sensor_data\";
+%filename = date + "_" + "sensor_data" + ".csv";
+%writetable(tab, fullfile(path,filename));
 
 %% add to file organization spreadsheet
-to_open = "data_organization_updated.csv";
+to_open = "final_data_organization.csv";
 path_open = "data_acquisition\";
 tab = table(filename, fault_level, drained, impulse_length);
 writetable(tab, to_open, 'WriteMode', 'append');
