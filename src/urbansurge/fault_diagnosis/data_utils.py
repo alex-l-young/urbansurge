5b4f033856d267f84e5b57303c171fd9c61f58be
Alex Young <45477688+alex-l-young@users.noreply.github.com>
2026-03-12 07:24:45 -0500

    Update data_utils.py
    Remove ANN dataset from data utils.

--- src/urbansurge/fault_diagnosis/data_utils.py
+++ src/urbansurge/fault_diagnosis/data_utils.py
@@ -2,23 +2,22 @@
 from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
-import torch
-from torch.utils.data import Dataset
 
-class ANNDataset(Dataset):
-    def __init__(self, data, labels, transform=None):
-        self.data = data
-        self.labels = labels
-        self.transform = transform
 
-    def __len__(self):
-        return len(self.labels)
-
-    def __getitem__(self, idx):
-        data = self.data[idx]
-        label = self.labels[idx]
-
-        return data, label
+# class ANNDataset(Dataset):
+#     def __init__(self, data, labels, transform=None):
+#         self.data = data
+#         self.labels = labels
+#         self.transform = transform
+#
+#     def __len__(self):
+#         return len(self.labels)
+#
+#     def __getitem__(self, idx):
+#         data = self.data[idx]
+#         label = self.labels[idx]
+#
+#         return data, label
 
 
 def normalize_residuals(swmm, residual_df, sensor_links):
